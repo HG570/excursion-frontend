c933f7a277ea3fa72c391548bf8cf2c4e2688d7d
HG570 <rafaelpinheirovgp@gmail.com>
2025-03-09 17:10:24 -0300

    chore: change url

--- src/app/config/api-endpoints.ts
+++ src/app/config/api-endpoints.ts
@@ -1,5 +1,5 @@
 export const API_ENDPOINTS = {
-    BASE_URL: 'https://didactic-space-journey-w66664p97xq254w4-3000.app.github.dev',
+    BASE_URL: 'https://website.com',
     EXCURSION: '/excursion',
     MANAGER: '/manager',
     SCHOOL: '/school'
